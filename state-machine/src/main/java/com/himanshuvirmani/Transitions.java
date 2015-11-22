32cfff4c9bd29e744f6c31c2a4583c4aaec18b62
Himanshu Virmani <himanshuvirmani@gmail.com>
2015-11-22 22:42:54 +0530

    wrote test cases around state machine, also added Transitions for easy single line work

---
+++ state-machine/src/main/java/com/himanshuvirmani/Transitions.java
@@ -0,0 +1,85 @@
+package com.himanshuvirmani;
+
+import lombok.Data;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by himanshu.virmani on 15/11/15.
+ */
+@Data
+public class Transitions<T, E> {
+
+    private T[] fromAny;
+
+    private T[] toAmong;
+
+    private E[] onEach;
+
+    private boolean ignore;
+
+    private Transition.onSuccessListener<T, E> onSuccessListener;
+
+    public Transitions(TransitionsBuilder<T, E> tseTransitionBuilder) {
+        this.fromAny = tseTransitionBuilder.fromAny;
+        this.toAmong = tseTransitionBuilder.toAmong;
+        this.onEach = tseTransitionBuilder.onEach;
+        this.onSuccessListener = tseTransitionBuilder.onSuccessListener;
+        this.ignore = tseTransitionBuilder.ignore;
+    }
+
+
+    public static class TransitionsBuilder<U, V> {
+
+        private WeakReference<StateMachine<U, V>> stateMachineWeakReference;
+
+        private U[] fromAny;
+
+        private U[] toAmong;
+
+        private V[] onEach;
+
+        private boolean ignore;
+
+        private Transition.onSuccessListener<U, V> onSuccessListener;
+
+        public TransitionsBuilder(StateMachine<U, V> stateMachine) {
+            stateMachineWeakReference = new WeakReference<StateMachine<U, V>>(stateMachine);
+        }
+
+        public TransitionsBuilder<U, V> fromAny(U... fromStates) {
+            fromAny = fromStates;
+            return this;
+        }
+
+        public TransitionsBuilder<U, V> toAmong(U... toStates) {
+            toAmong = toStates;
+            return this;
+        }
+
+        public TransitionsBuilder<U, V> on(V... onEvents) {
+            onEach = onEvents;
+            return this;
+        }
+
+        public TransitionsBuilder<U, V> ignore() {
+            ignore=  true;
+            return this;
+        }
+
+        public TransitionsBuilder<U, V> setOnSuccessListener(Transition.onSuccessListener<U, V> onSuccessListener) {
+            this.onSuccessListener = onSuccessListener;
+            return this;
+        }
+
+        public StateMachine<U, V> create() {
+            final StateMachine<U, V> stateMachine = stateMachineWeakReference.get();
+            if(stateMachine != null) {
+                stateMachine.apply(new Transitions<U, V>(this));
+                return stateMachine;
+            }
+            return null;
+        }
+    }
+
+}
---
+++ state-machine/src/test/java/StateMachineTest.java
@@ -0,0 +1,154 @@
+import TestUtil.MySampleEvent;
+import TestUtil.MySampleState;
+import com.himanshuvirmani.StateMachine;
+import com.himanshuvirmani.Transition;
+import com.himanshuvirmani.exceptions.TransitionCreationException;
+import com.himanshuvirmani.exceptions.TransitionException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by himanshu.virmani on 22/11/15.
+ */
+public class StateMachineTest {
+
+    StateMachine<MySampleState, MySampleEvent> stateMachine;
+
+    @Before
+    public void init() {
+        stateMachine =
+                new StateMachine<MySampleState, MySampleEvent>(MySampleState.CREATED);
+    }
+
+
+    @Test
+    public void testSingleSuccessTransition() {
+
+        stateMachine.setStateChangeListener(new StateMachine.StateChangeListener<MySampleState, MySampleEvent>() {
+            @Override
+            public void onStateChanged(MySampleState from, MySampleState to, MySampleEvent on) {
+                assertTrue(from == MySampleState.CREATED);
+                assertTrue(to == MySampleState.ONHOLD);
+                assertTrue(on == MySampleEvent.HOLD);
+            }
+        });
+
+        stateMachine.transition().from(MySampleState.CREATED).to(MySampleState.ONHOLD).on(MySampleEvent.HOLD).setOnSuccessListener(new Transition.onSuccessListener<MySampleState, MySampleEvent>() {
+            @Override
+            public void onSuccess(MySampleState from, MySampleState to, MySampleEvent on) {
+                assertTrue(from == MySampleState.CREATED);
+                assertTrue(to == MySampleState.ONHOLD);
+                assertTrue(on == MySampleEvent.HOLD);
+            }
+        }).create();
+        stateMachine.transition().from(MySampleState.ONHOLD).to(MySampleState.DELIVERED).on(MySampleEvent.DELIVER).create();
+        stateMachine.transition().from(MySampleState.DELIVERED).on(MySampleEvent.CANCEL).ignore().create();
+
+        stateMachine.fire(MySampleEvent.HOLD);
+
+    }
+
+
+    @Test
+    public void testTransitionExceptionCase() {
+
+        stateMachine.transition().from(MySampleState.CREATED).to(MySampleState.ONHOLD).on(MySampleEvent.HOLD).setOnSuccessListener(new Transition.onSuccessListener<MySampleState, MySampleEvent>() {
+            @Override
+            public void onSuccess(MySampleState from, MySampleState to, MySampleEvent on) {
+
+            }
+        }).create();
+        stateMachine.transition().from(MySampleState.ONHOLD).to(MySampleState.DELIVERED).on(MySampleEvent.DELIVER).create();
+        stateMachine.transition().from(MySampleState.DELIVERED).on(MySampleEvent.CANCEL).ignore().create();
+
+        try {
+            stateMachine.fire(MySampleEvent.DELIVER);
+        } catch (TransitionException e) {
+            assertEquals(stateMachine.getCurrentState(), MySampleState.CREATED);
+        }
+    }
+
+    @Test
+    public void testIgnoreCase() {
+
+        stateMachine.transition().from(MySampleState.CREATED).to(MySampleState.ONHOLD).on(MySampleEvent.HOLD).setOnSuccessListener(new Transition.onSuccessListener<MySampleState, MySampleEvent>() {
+            @Override
+            public void onSuccess(MySampleState from, MySampleState to, MySampleEvent on) {
+
+            }
+        }).create();
+        stateMachine.transition().from(MySampleState.ONHOLD).to(MySampleState.DELIVERED).on(MySampleEvent.DELIVER).create();
+        stateMachine.transition().from(MySampleState.DELIVERED).on(MySampleEvent.CANCEL).ignore().create();
+
+        stateMachine.fire(MySampleEvent.HOLD);
+        stateMachine.fire(MySampleEvent.DELIVER);
+        stateMachine.fire(MySampleEvent.CANCEL);
+        assertEquals(stateMachine.getCurrentState(), MySampleState.DELIVERED);
+    }
+
+    @Test
+    public void testMultipleTransitionsInOneSuccess() {
+
+        stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD)
+                .toAmong(MySampleState.ONHOLD, MySampleState.DELIVERED).on(MySampleEvent.HOLD, MySampleEvent.DELIVER).create();
+
+        stateMachine.fire(MySampleEvent.HOLD);
+
+        assertEquals(stateMachine.getCurrentState(), MySampleState.ONHOLD);
+    }
+
+    @Test
+    public void testMultipleIgnoreTransitionsSuccess() {
+
+        stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD).on(MySampleEvent.HOLD, MySampleEvent.DELIVER).ignore().create();
+
+        stateMachine.fire(MySampleEvent.HOLD);
+
+        assertEquals(stateMachine.getCurrentState(), MySampleState.CREATED);
+    }
+
+    @Test
+    public void testSingleEventMultipleFromIgnoreSuccess() {
+
+        stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD).on(MySampleEvent.DELIVER).ignore().create();
+
+        stateMachine.fire(MySampleEvent.DELIVER);
+
+        assertEquals(stateMachine.getCurrentState(), MySampleState.CREATED);
+    }
+
+    @Test
+    public void testNoIgnoreNoToFailure() {
+
+        try {
+            stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD).on(MySampleEvent.DELIVER).create();
+            assertEquals(true, false);
+        } catch (Exception e) {
+            assertTrue(e instanceof TransitionCreationException);
+        }
+    }
+
+    @Test
+    public void testWrongFromToArgumentsFailure() {
+        try {
+            stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD)
+                    .toAmong(MySampleState.ONHOLD, MySampleState.DELIVERED, MySampleState.CANCELLED).on(MySampleEvent.DELIVER).create();
+            assertEquals(true, false);
+        } catch (Exception e) {
+            assertTrue(e instanceof TransitionCreationException);
+        }
+    }
+
+    @Test
+    public void testSingleEventMultipleFromToArgumentsSuccess() {
+        stateMachine.transitions().fromAny(MySampleState.CREATED, MySampleState.ONHOLD)
+                .toAmong(MySampleState.ONHOLD, MySampleState.DELIVERED).on(MySampleEvent.DELIVER).create();
+
+        stateMachine.fire(MySampleEvent.DELIVER);
+
+        assertEquals(stateMachine.getCurrentState(), MySampleState.ONHOLD);
+    }
+}
